b9999a5963880fe76555aeb706bf358dce2f6ff8
hassanShakur <hass.shakurhassan@gmail.com>
2023-06-08 18:19:24 +0300

    Success reorder bottom-up after drop

--- src/modules/dragDrop.js
+++ src/modules/dragDrop.js
@@ -20,7 +20,6 @@ export const getTaskBelow = (mousePosition) => {
 };
 
 const reorderTopDown = (id1, id2, tasks) => {
-  // console.log(id1, id2);
   const newTasks = [...tasks];
   for (let i = id1; i < id2 - 1; i++) {
     newTasks[i].index -= 1;
@@ -28,15 +27,23 @@ const reorderTopDown = (id1, id2, tasks) => {
   newTasks[id1 - 1].index = id2 - 1;
   return newTasks;
 };
-const reorderBottomUp = () => {};
+
+const reorderBottomUp = (id1, id2, tasks) => {
+  const newTasks = [...tasks];
+  newTasks[id1 - 1].index = id2;
+  for (let i = id2 - 1; i < id1 - 1; i++) {
+    newTasks[i].index += 1;
+  }
+  return newTasks;
+};
 
 export const reorderTasks = (droppedTaskId, taskBelowId, tasks) => {
   // Item was dropped in original position
-  if (droppedTaskId === taskBelowId - 1) return;
+  if (droppedTaskId === taskBelowId - 1) return tasks;
   const newTasks =
     droppedTaskId < taskBelowId
       ? reorderTopDown(droppedTaskId, taskBelowId, tasks)
-      : reorderBottomUp();
+      : reorderBottomUp(droppedTaskId, taskBelowId, tasks);
 
   return newTasks;
 };
